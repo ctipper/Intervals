c4cdf2d80a4e3a7a0233fecb708ef9c8838503eb
ctipper <chris.tipper@gmail.com>
2020-12-13 10:19:11 +0000

    Initial Commit

---
+++ Intervals/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Intervals
+//
+//  Created by Christopher on 13-12-2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
